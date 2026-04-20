5b96d7f2a505c05628c553a42e5257cc1a20dea8
Sergio <Sergio.carbonell@opendeusto.es>
2026-04-20 22:18:14 +0200

    E2: conexion de modelos con vistas y sistema de navegacion funcional

--- mysite/compras/views.py
+++ mysite/compras/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from .models import Articulo, OrdenCompra
 
 def inicio(request):
     return render(request, 'compras/index.html')
 
 def listado_ordenes(request):
     
-    return render(request, 'compras/listado_ordenes.html')
+    todas_ordenes = OrdenCompra.objects.all()
+    
+   
+    return render(request, 'compras/listado_ordenes.html', {'ordenes': todas_ordenes})
 
 def listado_articulos(request):
-    return render(request, 'compras/listado_articulos.html')
+    
+    productos = Articulo.objects.all() 
+    
+   
+    return render(request, 'compras/listado_articulos.html', {'articulos': productos})
